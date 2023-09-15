ad6ee3ede9c5a5bd08ed7911b5d5a9f2920166c8
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-15 23:43:13 +0530

    Median of Two Sorted Array

--- Array/Median of Two Sorted Array.java
+++ Array/Median of Two Sorted Array.java
@@ -16,7 +16,6 @@ nums2.length == n
 1 <= m + n <= 2000
 -106 <= nums1[i], nums2[i] <= 106
 */
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m=nums1.length;
