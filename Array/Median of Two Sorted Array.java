81ba6af5383ff74ae5d746c5f318aeb4bd54ed27
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-15 23:42:13 +0530

    Median of Two Sorted Array.

--- Array/Median of Two Sorted Array.java
+++ Array/Median of Two Sorted Array.java
@@ -8,7 +8,6 @@ Example 2:
 Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
 Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
- 
 Constraints:
 nums1.length == m
 nums2.length == n
