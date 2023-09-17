21bdb2ebaeb7040b77fd8889abc267e415988220
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-17 23:54:31 +0530

    3Sum

--- Array/3Sum.java
+++ Array/3Sum.java
@@ -1,12 +1,7 @@
 /* 
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-
 Notice that the solution set must not contain duplicate triplets.
-
- 
-
 Example 1:
-
 Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
 Explanation: 
@@ -16,7 +11,6 @@ nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 The distinct triplets are [-1,0,1] and [-1,-1,2].
 Notice that the order of the output and the order of the triplets does not matter.
 Example 2:
-
 Input: nums = [0,1,1]
 Output: []
 Explanation: The only possible triplet does not sum up to 0.
