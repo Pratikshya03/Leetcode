a47e58ba3622adda761386a228cef0459d6782b7
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-22 23:31:10 +0530

    3Sum

--- Array/3Sum.java
+++ Array/3Sum.java
@@ -21,9 +21,7 @@ Explanation: The only possible triplet sums up to 0.
 Constraints:
 3 <= nums.length <= 3000
 -105 <= nums[i] <= 105
-
- */
-
+*/
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
