212d71e88f37b35c9ec9d0e0893807aa92329734
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-25 00:19:44 +0530

    4Sum

--- Array/4Sum.java
+++ Array/4Sum.java
@@ -1,12 +1,8 @@
 /*Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
-
-0 <= a, b, c, d < n
+0 <= a, b, c, d < n 
 a, b, c, and d are distinct.
 nums[a] + nums[b] + nums[c] + nums[d] == target
 You may return the answer in any order.
-
- 
-
 Example 1:
 
 Input: nums = [1,0,-1,0,-2,2], target = 0
