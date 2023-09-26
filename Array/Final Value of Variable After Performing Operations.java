a7318cf3ebd6ea46550a8e07d5b52dbbc1b32a81
Pratikshya03 <131337324+Pratikshya03@users.noreply.github.com>
2023-09-26 20:18:26 +0530

    Final Value of Variable After Performing Operations

--- Array/Final Value of Variable After Performing Operations.java
+++ Array/Final Value of Variable After Performing Operations.java
@@ -18,7 +18,6 @@ Initially, X = 0.
 ++X: X is incremented by 1, X = 1 + 1 = 2.
 X++: X is incremented by 1, X = 2 + 1 = 3.
 Example 3:
-
 Input: operations = ["X++","++X","--X","X--"]
 Output: 0
 Explanation: The operations are performed as follows:
@@ -27,9 +26,7 @@ X++: X is incremented by 1, X = 0 + 1 = 1.
 ++X: X is incremented by 1, X = 1 + 1 = 2.
 --X: X is decremented by 1, X = 2 - 1 = 1.
 X--: X is decremented by 1, X = 1 - 1 = 0.
-Constraints:
-
-1 <= operations.length <= 100
+Constraints: 1 <= operations.length <= 100
 operations[i] will be either "++X", "X++", "--X", or "X--". */
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
